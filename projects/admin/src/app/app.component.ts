bdd194e06800b9cf2e5c173ba3054b395cfc4d2e
Bhavna Dhankarghare <roshand.ranjan@st.niituniversity.in>
2021-05-12 10:58:27 +0530

    #Admin - Configure login ui

--- projects/admin/src/app/app.component.ts
+++ projects/admin/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppSettings } from '../../../../src/app/app.settings';
+import { Settings } from '../../../../src/app/app.settings.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public settings: Settings;
+    constructor(public appSettings:AppSettings, private router:Router){
+        this.settings = this.appSettings.settings;  
+    }  
   title = 'Admin';
 }
--- projects/admin/src/app/app.module.ts
+++ projects/admin/src/app/app.module.ts
@@ -3,30 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './Shared/Components/header/header.component';
-import { SidebarComponent } from './Shared/Components/sidebar/sidebar.component';
-import { DashboardComponent } from './Pages/full-page/dashboard/dashboard.component';
-import { EmailTemplateComponent } from './Pages/full-page/email-template/email-template.component';
-import { LoginComponent } from './Pages/content/login/login.component';
-import { RegisterComponent } from './Pages/content/register/register.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './Shared/login/login.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppSettings } from '../../../../src/app/app.settings';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SidebarComponent,
-    DashboardComponent,
-    EmailTemplateComponent,
-    LoginComponent,
-    RegisterComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    AppSettings,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
